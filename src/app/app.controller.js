a68be7e3f2cbc7d89364c965c9dd20f0db621f25
Christopher Fisher <chris@alphafitch.uk>
2016-03-15 17:10:30 +0000

    Moving the config json data into a new location, updated http getter to match

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,7 +1,7 @@
 carousel.controller('appController', function($scope,  $http) {
 
     // Get the list of iframes to display in the carousel
-    $http.get("src/app/components/frame/frames.json").then(function(response) {
+    $http.get("src/app/app.config.json").then(function(response) {
         $scope.frames = response.data.list;
     });
 
