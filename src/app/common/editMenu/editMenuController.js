42a1161758b06c9b706e054a33b4b488dd15c5c7
Christopher Fisher <chris.d.fisher@directlinegroup.co.uk>
2016-03-15 16:04:24 +0000

    Removed functions that aren't used in the edit menu and tidied up some extra whitespace

--- src/app/common/editMenu/editMenuController.js
+++ src/app/common/editMenu/editMenuController.js
@@ -1,20 +1,11 @@
 carousel.controller("editMenuController", function($scope, $mdSidenav) {
 
     // Generic function to build a toggler that opens/closes the sidenavs
-    $scope.buildToggler = function(navID){
+    $scope.buildToggler = function(navID) {
         return function() {
             $mdSidenav(navID).toggle();
         };
     };
 
     $scope.toggleEditMenu = $scope.buildToggler("editMenu");
-
-    $scope.isOpenLeft = function(){
-        return $mdSidenav("editMenu").isOpen();
-    };
-
-    $scope.close = function () {
-        $mdSidenav("editMenu").close();
-    };
-
 });
