c825f0c41054c52998663389f843e8cecc7d00fc
Christopher Fisher <chris@alphafitch.uk>
2016-03-17 07:43:52 +0000

    Changed display time editor to a slider instead of drop down, removed the timerOptions variable which is no longer needed

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,8 +1,5 @@
 carousel.controller("appController", function($scope,  $http) {
 
-    // The list of available options when selecting the timer
-    $scope.timerOptions = ["10", "100"];
-
     // Get the list of iframes to display in the carousel
     $http.get("src/app/app.config.json").then(function(response) {
         $scope.frames = response.data.list;
