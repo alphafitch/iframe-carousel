67a3e94299deccd25dc5ee07c80ef5230f1bed9a
Christopher Fisher <chris@alphafitch.uk>
2016-03-22 18:58:54 +0000

    Adding the version number to the title page with a link to the github repo, updated the version numbers in package.json/bower.json to match actual version number

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,5 +1,10 @@
 carousel.controller("appController", function($scope,  $http) {
 
+    // Required for version info from the bower file
+    $http.get('bower.json').then(function(response) {
+        $scope.bower = response.data;
+    });
+
     // Get the list of iframes to display in the carousel
     $http.get("src/app/app.config.json").then(function(response) {
         $scope.frames = response.data.list;
